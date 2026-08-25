47bab4f7550040d35b9359bfe401b84b76f8a142
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-08-25 15:56:00 -0600

    Estructura básica

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/themed-text";
+import React from "react";
+import { View } from "react-native";
+
+const PushApp = () => {
+  return (
+    <View>
+      <ThemedText>PushApp</ThemedText>
+    </View>
+  );
+};
+
+export default PushApp;
